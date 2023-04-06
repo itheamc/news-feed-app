a8f947a8673dc747123cd6e9c643636ebda9e949
Amit Chaudhary <itheamc@gmail.com>
2023-04-06 07:18:11 +0545

    Added models for register and login response handling

---
+++ app/src/main/java/com/itheamc/newsfeedappnchl/data/models/LoginResponseEntity.kt
@@ -0,0 +1,6 @@
+package com.itheamc.newsfeedappnchl.data.models
+
+data class LoginResponseEntity(
+    val user: User?,
+    val message: String?
+)
---
+++ app/src/main/java/com/itheamc/newsfeedappnchl/data/models/RegisterResponseEntity.kt
@@ -0,0 +1,6 @@
+package com.itheamc.newsfeedappnchl.data.models
+
+data class RegisterResponseEntity(
+    val user: User?,
+    val message: String?
+)
